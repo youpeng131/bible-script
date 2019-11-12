77e44a8f97bdc9d4855b8d3ab4329a2abb520156
jindouyun123 <youpeng131@163.com>
2019-11-12 17:41:07 +0800

    first commit

--- api/thing/thing.controller.js
+++ api/thing/thing.controller.js
@@ -13,134 +13,150 @@ const _ = require('lodash');
 
 
 module.exports.findAll = function (req, res) {
-    let arr = [];
-    // 获取token
-    let url = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=GGcpZzvzYIPfhNScNRP1tZyE&client_secret=ovQeQOVn4sAO0ihZKLwgDX4Op22ifSSm&';
-    request(url, function (error, response, body) {
+
+
+    request('http://hezuo.kanshu.cn/newoffer/booklist.php?cono=100530', function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
 
-            body = JSON.parse(body);
+            // body = JSON.parse(body);
             console.log(body);
-            if (body.access_token) {
-
-                // 发起处理请求URL
-                let baidubce = 'https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer?charset=UTF-8&access_token=' + body.access_token + ''
-
-                // 读取需要处理文章
-                fs.readFile('./sqldb/霸仙绝杀.txt', function (err, data) {
-                    // 读取文件失败/错误
-                    if (err) {
-                        throw err;
-                    }
-                    // 读取文件成功
-                    // 根据总字数算出总共需要请求多少次
-                    // 每2w个字符算请求一次
-                    let ret = data.toString();
-                    let Remainder = ret.length / 10000;
-                    console.log(Remainder);
-                    var i = 0, howManyTimes = Remainder;
-                    // 递归调用每隔5s调用一次请求
-                    function f() {
-
-                        let sub = ret.substring(i * 10000, (i * 10000) + 10000);
-                        request({
-                            url: baidubce,
-                            method: "POST",
-                            json: true,
-                            headers: {
-                                "content-type": "application/json",
-                            },
-                            body: {
-                                "text": sub
-                            }
-                        }, function (error, response, body) {
-
-                            if (!error && response.statusCode == 200) {
-
-                                //console.log(body);
-                                for (let i = 0; i < body.items.length; i++) {
-                                    if (body.items[i].ne === 'PER' ||
-                                        body.items[i].ne === 'LOC' ||
-                                        body.items[i].ne === 'ORG' ||
-                                        body.items[i].ne === 'YY' ||
-                                        body.items[i].ne === 'WLYY' ||
-                                        body.items[i].ne === 'CNSH'
-
-
-                                    ) {
-                                        arr.push({ name: body.items[i].item, ne: body.items[i].ne });
-                                    }
-
-                                }
-
-
-                            }
-                        })
-                        i++;
-                        if (i <= howManyTimes) {
-                            console.log(i);
-                            setTimeout(f, 1000);
-                        } else {
-
-
-                            // 去重统计
-                            function countId(data) {
-                                var count = {};
-                                for (var x = 0; x < data.length; x++) {
-                                    if (count[data[x].name]) {
-                                        count[data[x].name]++;
-                                        continue;
-                                    }
-                                    count[data[x].name] = 1;
-                                }
-                                return count;
-                            }
-                            var countResult = countId(arr);
-
-                            // 去重
-                            function RemoveArr(result) {
-                                for (var x = 0; x < result.length; x++) {
-                                    for (var y = x + 1; y < result.length;) {
-                                        if (result[x].name === result[y].name) {
-                                            result.splice(y, 1)
-                                        } else {
-                                            y++
-                                        }
-                                    }
-                                }
-                                return result
-                            }
-
-
-
-                            let r = RemoveArr(arr);
-                            // 比对 加入count字段
-                            for (let index = 0; index < r.length; index++) {
-                                for (var j in countResult) {
-                                    if (r[index].name === j) {
-                                        r[index]['count'] = countResult[j];
-                                    }
-                                }
-
-                            }
-
-
-                            _.orderBy(r, ['count'], ['desc']);
-                            console.log('stop');
-                            // 生成excel
-                            dlXlsx(r);
-                            // 强行终止
-                            setTimeout(arguments.callee, 1000);
-                        }
-                    }
-                    f();
-                });
-
-
-            }
         }
     })
+
+
+
+    // let arr = [];
+    // // 获取token
+    // let url = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=GGcpZzvzYIPfhNScNRP1tZyE&client_secret=ovQeQOVn4sAO0ihZKLwgDX4Op22ifSSm&';
+    // request(url, function (error, response, body) {
+
+    //     if (!error && response.statusCode == 200) {
+
+    //         body = JSON.parse(body);
+    //         console.log(body);
+    //         if (body.access_token) {
+
+    //             // 发起处理请求URL
+    //             let baidubce = 'https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer?charset=UTF-8&access_token=' + body.access_token + ''
+
+    //             // 读取需要处理文章
+    //             fs.readFile('./sqldb/霸仙绝杀.txt', function (err, data) {
+    //                 // 读取文件失败/错误
+    //                 if (err) {
+    //                     throw err;
+    //                 }
+    //                 // 读取文件成功
+    //                 // 根据总字数算出总共需要请求多少次
+    //                 // 每2w个字符算请求一次
+    //                 let ret = data.toString();
+    //                 let Remainder = ret.length / 10000;
+    //                 console.log(Remainder);
+    //                 var i = 0, howManyTimes = Remainder;
+    //                 // 递归调用每隔5s调用一次请求
+    //                 function f() {
+
+    //                     let sub = ret.substring(i * 10000, (i * 10000) + 10000);
+    //                     request({
+    //                         url: baidubce,
+    //                         method: "POST",
+    //                         json: true,
+    //                         headers: {
+    //                             "content-type": "application/json",
+    //                         },
+    //                         body: {
+    //                             "text": sub
+    //                         }
+    //                     }, function (error, response, body) {
+
+    //                         if (!error && response.statusCode == 200) {
+
+    //                             //console.log(body);
+    //                             for (let i = 0; i < body.items.length; i++) {
+    //                                 if (body.items[i].ne === 'PER' ||
+    //                                     body.items[i].ne === 'LOC' ||
+    //                                     body.items[i].ne === 'ORG' ||
+    //                                     body.items[i].ne === 'YY' ||
+    //                                     body.items[i].ne === 'WLYY' ||
+    //                                     body.items[i].ne === 'CNSH'
+
+
+    //                                 ) {
+    //                                     arr.push({ name: body.items[i].item, ne: body.items[i].ne });
+    //                                 }
+
+    //                             }
+
+
+    //                         }
+    //                     })
+    //                     i++;
+    //                     if (i <= howManyTimes) {
+    //                         console.log(i);
+    //                         setTimeout(f, 1000);
+    //                     } else {
+
+
+    //                         // 去重统计
+    //                         function countId(data) {
+    //                             var count = {};
+    //                             for (var x = 0; x < data.length; x++) {
+    //                                 if (count[data[x].name]) {
+    //                                     count[data[x].name]++;
+    //                                     continue;
+    //                                 }
+    //                                 count[data[x].name] = 1;
+    //                             }
+    //                             return count;
+    //                         }
+    //                         var countResult = countId(arr);
+
+    //                         // 去重
+    //                         function RemoveArr(result) {
+    //                             for (var x = 0; x < result.length; x++) {
+    //                                 for (var y = x + 1; y < result.length;) {
+    //                                     if (result[x].name === result[y].name) {
+    //                                         result.splice(y, 1)
+    //                                     } else {
+    //                                         y++
+    //                                     }
+    //                                 }
+    //                             }
+    //                             return result
+    //                         }
+
+
+
+    //                         let r = RemoveArr(arr);
+    //                         // 比对 加入count字段
+    //                         for (let index = 0; index < r.length; index++) {
+    //                             for (var j in countResult) {
+    //                                 if (r[index].name === j) {
+    //                                     r[index]['count'] = countResult[j];
+    //                                 }
+    //                             }
+
+    //                         }
+
+
+    //                         _.orderBy(r, ['count'], ['desc']);
+    //                         console.log('stop');
+    //                         // 生成excel
+    //                         dlXlsx(r);
+    //                         // 强行终止
+    //                         setTimeout(arguments.callee, 1000);
+    //                     }
+    //                 }
+    //                 f();
+    //             });
+
+
+    //         }
+    //     }
+    // })
+
+
+
 
 
 
