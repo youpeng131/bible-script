3c3fc16e4d992df8960451095b5dfa5baae6c328
jindouyun123 <youpeng131@163.com>
2019-11-14 15:09:04 +0800

    save

--- api/thing/thing.controller.js
+++ api/thing/thing.controller.js
@@ -59,7 +59,7 @@ module.exports.findAll = function (req, res) {
             if (!error && response.statusCode == 200) {
 
                 xml2js(body, { explicitArray: false, ignoreAttrs: true }, function (err, result) {
-                    console.log(result);
+                    console.log(result.chapters.chapter.chapterid, result.chapters.chapter.content);
                     // _.map(result.chapters.chapter, function (item) {
                     //     console.log(item.chapterid, item.content);
                     // })
